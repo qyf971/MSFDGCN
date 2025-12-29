bad5080a4b35e6e4d4de95787bcf4080eec452ab
yufeiqian794 <yufeiqian794@163.com>
2025-12-29 22:52:41 +0800

    update

---
+++ scripts/model_L96.sh
@@ -0,0 +1,15 @@
+# dataset
+python -u exp_baselines.py \
+  --seq_len 96 \
+  --pred_len 96 \
+  --target_state "California_cities"
+
+python -u exp_baselines.py \
+--seq_len 96 \
+--pred_len 96 \
+--target_state "Illinois_cities"
+
+python -u exp_baselines.py \
+  --seq_len 96 \
+  --pred_len 96 \
+  --target_state "Texas_cities"
